de41215d3cd0da726f37003d9dacff46d8760031
sam.perrin <samtperrin@gmail.com>
2019-02-16 09:48:35 +0000

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 docker build -t docker-term .
-docker stop docker-term
-docker rm docker-term
+docker stop docker-term || true && docker rm docker-term || true
 docker run -p 1000:8080 -d -v /var/run/docker.sock:/run/docker.sock --name docker-term docker-term
